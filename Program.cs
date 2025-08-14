ab4a2de4a13d75135553a6e05ba9611a639b44ce
lopeze25 <112291194+lopeze25@users.noreply.github.com>
2025-08-14 14:40:22 -0500

    Fixed document root

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseStaticFiles();
+//  wwwroot/index.html at "/"
+app.UseDefaultFiles();   // index.html, default.htm, etc.
+app.UseStaticFiles();    // static files from wwwroot
+
+
+app.MapGet("/api/hello", () => "Hello from API"); // move off "/"
 
 app.Run();
